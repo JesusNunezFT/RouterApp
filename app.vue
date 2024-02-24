eecbb2e69fd3a28a478165e9c56321a3e08642fd
Jesus Nuñez <jesus.nunez@theconsultingcompany.eu>
2024-02-23 22:09:56 -0500

    primevue funcionando y eliminando app.vue

--- app.vue
+++ app.vue
@@ -1,5 +1,21 @@
+<script>
+import 'primevue/resources/themes/aura-light-green/theme.css';
+
+import 'primeicons/primeicons.css'; // Agrega esta línea si estás utilizando iconos de PrimeIcons
+import { Button } from 'primevue/button'; // Importa el componente Button de PrimeVue
+
+
+export default {
+  components: {
+    Button // Registra el componente Button para su uso en tu plantilla
+  }
+}
+</script>
+
 <template>
   <div>
     <p>Hola desde Nuxt3</p>
+
+    <Button label="Check" icon="pi pi-check" />
   </div>
 </template>
--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,4 +1,25 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
+
 export default defineNuxtConfig({
-  
+  devtools: { enabled: true },
+ 
+  typescript: {
+    typeCheck: false
+  },
+
+  modules: [
+    'nuxt-primevue'
+  ],
+
+//   primevue: {
+//     options: {
+//         unstyled: true,
+//         ripple: true,
+//         inputStyle: 'filled'
+//     },
+// },
+
+  css: ['primevue/resources/themes/aura-light-green/theme.css'],
+ 
+
 })
